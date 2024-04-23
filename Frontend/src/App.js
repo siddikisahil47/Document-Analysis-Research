615bef69dc05c94670f9774751d60011afde305f
siddikisahi47 <siddikisahil47@gmail.com>
2024-04-23 19:26:06 +0530

    Create Frontend
    Frontend without CSS

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DocAnalysis from './components/documentAnalysis/DocAnalysis';
+
+function App() {
+  return (
+    <div className="App">
+      <DocAnalysis />
+    </div>
+  );
+}
+
+export default App;
